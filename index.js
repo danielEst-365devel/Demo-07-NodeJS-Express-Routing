21d66142f0a89de8210c63ab35c89d52858bab3f
Daniel <daniel.anth.est.03@gmail.com>
2024-11-05 14:39:41 +0800

    Modular Approach

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 
 const app = express();
 const PORT = 4000;
@@ -9,65 +10,16 @@ app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
-// Customers
-app.get('/customers', (request, response) => {
-    response.status(200).json({ message: 'GET all customers' });
-});
+// Customers  
+const customerRouter = require('./Routes/customer');  
+app.use('/customers', customerRouter);  
 
-app.post('/customers', (request, response) => {
-    response.status(200).json({ message: `CREATE new customer` });
-});
+// Orders  
+const orderRouter = require('./Routes/orders');  
+app.use('/orders', orderRouter);  
 
-app.get('/customers/:id', (request, response) => {
-    response.status(200).json({ message: `GET customer with id ${request.params.id}` });
-});
+// Payments  
+const paymentRouter = require('./Routes/payment');  
+app.use('/payments', paymentRouter);
 
-app.put('/customers/:id', (request, response) => {
-    response.status(200).json({ message: `UPDATE customer with id ${request.params.id}` });
-});
-
-app.delete('/customers/:id', (request, response) => {
-    response.status(200).json({ message: `DELETE customer with id ${request.params.id}` });
-});
-
-// Orders
-app.get('/orders', (request, response) => {
-    response.status(200).json({ message: 'GET all orders' });
-});
-
-app.post('/orders', (request, response) => {
-    response.status(200).json({ message: `CREATE new order` });
-});
-
-app.get('/orders/:id', (request, response) => {
-    response.status(200).json({ message: `GET order with id ${request.params.id}` });
-});
-
-app.put('/orders/:id', (request, response) => {
-    response.status(200).json({ message: `UPDATE order with id ${request.params.id}` });
-});
-
-app.delete('/orders/:id', (request, response) => {
-    response.status(200).json({ message: `DELETE order with id ${request.params.id}` });
-});
-
-// Payments
-app.get('/payments', (request, response) => {
-    response.status(200).json({ message: 'GET all payments' });
-});
-
-app.post('/payments', (request, response) => {
-    response.status(200).json({ message: `CREATE new payments` });
-});
-
-app.get('/payments/:id', (request, response) => {
-    response.status(200).json({ message: `GET payments with id ${request.params.id}` });
-});
-
-app.put('/payments/:id', (request, response) => {
-    response.status(200).json({ message: `UPDATE payments with id ${request.params.id}` });
-});
-
-app.delete('/payments/:id', (request, response) => {
-    response.status(200).json({ message: `DELETE payments with id ${request.params.id}` });
-});
+module.exports = router;
